056e0b0e7db4fa881bd02b91b1b72b2d49d40166
Jens Kalshoven <jens.kalshoven@gmail.com>
2023-12-13 14:20:56 +0100

    Day 13

---
+++ src/bin/day13.rs
@@ -0,0 +1,102 @@
+use std::cmp::min;
+use advent_of_code_2023::{count_different_elements, transpose};
+
+type Pattern = Vec<Vec<char>>;
+
+fn main() {
+    let input = parse(include_str!("../../data/day13/input.txt"));
+
+    puzzle_1(&input);
+    puzzle_2(&input);
+}
+
+fn parse(raw_input: &str) -> Vec<Pattern>
+{
+    raw_input
+        .split("\n\n")
+        .map(|block| block.lines().map(|l| l.chars().collect()).collect())
+        .collect()
+}
+
+fn puzzle_1(data: &[Pattern]) -> usize {
+    data
+        .iter()
+        .map(|p| {
+            match find_mirror_position(p) {
+                Some(position) => position * 100,
+                None => find_mirror_position(&transpose(p)).unwrap_or(0),
+            }
+        })
+        .sum()
+}
+
+fn puzzle_2(data: &[Pattern]) -> usize {
+    data
+        .iter()
+        .map(|p| {
+            match find_mirror_position_with_smudge(p) {
+                Some(position) => position * 100,
+                None => find_mirror_position_with_smudge(&transpose(p)).unwrap_or(0),
+            }
+        })
+        .sum()
+}
+
+fn find_mirror_position(pattern: &Pattern) -> Option<usize> {
+    (1..pattern.len())
+        .into_iter()
+        .find(|&position| (1..=min(position, pattern.len() - position))
+            .into_iter()
+            .all(|offset| pattern[position - offset] == pattern[position + offset - 1])
+        )
+}
+
+fn find_mirror_position_with_smudge(pattern: &Pattern) -> Option<usize> {
+    (1..pattern.len())
+        .into_iter()
+        .find(|&position| (1..=min(position, pattern.len() - position))
+            .into_iter()
+            .try_fold(false, | found_smudge, offset | {
+                let diff = count_different_elements(&pattern[position - offset], &pattern[position + offset - 1]);
+                match (diff, found_smudge) {
+                    (0, _) => Some(found_smudge),
+                    (1, false) => Some(true),
+                    _ => None,
+                }
+            })
+            .unwrap_or(false)
+        )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_puzzle_1_example() {
+        let input = parse(include_str!("../../data/day13/input_example.txt"));
+
+        assert_eq!(puzzle_1(&input), 405);
+    }
+
+    #[test]
+    fn test_puzzle_1() {
+        let input = parse(include_str!("../../data/day13/input.txt"));
+
+        assert_eq!(puzzle_1(&input), 37381);
+    }
+
+    #[test]
+    fn test_puzzle_2_example() {
+        let input = parse(include_str!("../../data/day13/input_example.txt"));
+
+        assert_eq!(puzzle_2(&input), 400);
+    }
+
+    #[test]
+    fn test_puzzle_2() {
+        let input = parse(include_str!("../../data/day13/input.txt"));
+
+        assert_eq!(puzzle_2(&input), 28210);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::slice::Iter;
+
 pub fn moore_neighborhood((y, x): (usize, usize), height: usize, width: usize) -> Vec<(usize, usize)> {
     [
         (y.wrapping_sub(1), x.wrapping_sub(1)),
@@ -12,4 +14,27 @@ pub fn moore_neighborhood((y, x): (usize, usize), height: usize, width: usize) -
         .into_iter()
         .filter(|&(ny, nx)| ny < height && nx < width)
         .collect()
+}
+
+pub fn transpose<T: Copy>(matrix: &Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let mut row_iters: Vec<Iter<T>> = matrix
+        .into_iter()
+        .map(|r| r.into_iter())
+        .collect();
+
+    (0..matrix.first().unwrap().len())
+        .map(|_| row_iters
+            .iter_mut()
+            .map(|it| it.next().unwrap().clone())
+            .collect::<Vec<T>>()
+        )
+        .collect()
+}
+
+pub fn count_different_elements<T: Eq>(a: &[T], b: &[T]) -> usize {
+    a
+        .iter()
+        .zip(b)
+        .filter(|&(element_a, element_b) | element_a != element_b)
+        .count()
 }
